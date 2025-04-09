1e2f99dc84cde6b1898196c116f40dbf3d7be826
weaverbo <kyeongboo@naver.com>
2025-04-09 14:13:56 +0900

    style : Heaer UI 구현, 글로벌 스타일 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,17 @@ module.exports = {
         InriaSans: ["var(--font-Inria_Sans)"],
       },
       fontSize: {
-        lg: "3.6rem",
-        md: "2.4rem",
+        xs: "1.4rem",    
+        sm: '1rem',     
+        base: '1.125rem',   
+        md: '1.25rem',    
+        lg: '1.3125rem',    
+        '2xl': '2.25rem',  
+        '3xl': '2.5rem',    
+        '4xl': '3rem',    
+        '5xl': '3.5rem',     
+        '6xl': '7.5rem',   
+        '7xl': '8rem',   
       },
       fontWeight: {
         normal: "300",
@@ -20,6 +29,8 @@ module.exports = {
       },  
       colors: {
        main : "#2640A1",
+       submain : "FFCD00",
+       background : "EBEBEB",
       },
     },
   },
