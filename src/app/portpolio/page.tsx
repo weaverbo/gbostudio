d4ed774979cd3b17a5a60722b5002bda1b1c9981
weaverbo <kyeongboo@naver.com>
2025-04-19 16:21:08 +0900

    style : portpolio 페이지 모바일 버전 반응형 스타일링 구현 완료

--- src/app/portpolio/page.tsx
+++ src/app/portpolio/page.tsx
@@ -13,48 +13,46 @@ import '../../styles/portfolio.css';
 
 export default function Portpolio() {
   return (
-    <div className="container">
-      <h1 className="text-[170px] text-main font-bold pt-[163px] ml-[-15.5px]">
-        PORTFOLIO
-      </h1>
-      <div className="portfolio-container mt-[80px]">
-        <div className="mt-[10px]">
+    <div className="container w-full">
+      <h1 className="portfolio-container-title">PORTFOLIO</h1>
+      <div className="portfolio-container">
+        <div className="portpolio-img-container">
           <Image
-            className="portpolio_img"
+            className="portpolio-img"
             src={team_portfolio}
             alt="team_portfolio"
           />
         </div>
         <div>
-          <h2 className="portpolio_title">Team Website</h2>
+          <h2 className="portpolio-title">Team Website</h2>
           <div className="portfolio-detail-container">
-            <span className="portpolio_tag w-[182px]">Website Design</span>
-            <span className="portpolio_tag w-[227px]">React Development</span>
-            <span className="portpolio_tag w-[109px]">Concept</span>
+            <span className="portpolio-tag w-[182px]">Website Design</span>
+            <span className="portpolio-tag w-[227px]">React Development</span>
+            <span className="portpolio-tag w-[109px]">Concept</span>
             <StyledLink
               href="/portpolio/portpolio_detail"
-              className="portpolio_link"
+              className="portpolio-link"
             >
               see this project
             </StyledLink>
           </div>
         </div>
       </div>
-      <div className="portfolio-container justify-end my-[36px]">
-        <div>
-          <h2 className="portpolio_title">Chat App Website</h2>
-          <div className="portfolio-detail-container items-end">
-            <span className="portpolio_tag w-[182px]">Website Design</span>
-            <span className="portpolio_tag w-[227px]">React Development</span>
-            <span className="portpolio_tag w-[109px]">Concept</span>
-            <StyledLink href="/" className="portpolio_link">
+      <div className="portfolio-container-other">
+        <div className="portfolio-contents-container">
+          <h2 className="portpolio-title">Chat App Website</h2>
+          <div className="portfolio-detail-container items-end flex-row-reverse">
+            <span className="portpolio-tag w-[182px]">Website Design</span>
+            <span className="portpolio-tag w-[227px]">React Development</span>
+            <span className="portpolio-tag w-[109px]">Concept</span>
+            <StyledLink href="/" className="portpolio-link">
               see this project
             </StyledLink>
           </div>
         </div>
         <div className="mt-[10px]">
           <Image
-            className="portpolio_img"
+            className="portpolio-img"
             src={chat_app_portfolio}
             alt="chat_app_portfolio"
           />
@@ -63,38 +61,38 @@ export default function Portpolio() {
       <div className="portfolio-container">
         <div className="mt-[10px]">
           <Image
-            className="portpolio_img"
+            className="portpolio-img"
             src={hotelbooking_portfolio}
             alt="hotelbooking_portfolio"
           />
         </div>
         <div>
-          <h2 className="portpolio_title">Hotel Booking Website</h2>
+          <h2 className="portpolio-title">Hotel Booking Website</h2>
           <div className="portfolio-detail-container">
-            <span className="portpolio_tag w-[182px]">Website Design</span>
-            <span className="portpolio_tag w-[227px]">React Development</span>
-            <span className="portpolio_tag w-[109px]">Concept</span>
-            <StyledLink href="/" className="portpolio_link">
+            <span className="portpolio-tag w-[182px]">Website Design</span>
+            <span className="portpolio-tag w-[227px]">React Development</span>
+            <span className="portpolio-tag w-[109px]">Concept</span>
+            <StyledLink href="/" className="portpolio-link">
               see this project
             </StyledLink>
           </div>
         </div>
       </div>
-      <div className="portfolio-container justify-end my-[36px]">
-        <div>
-          <h2 className="portpolio_title ">ProYoga Buddy Website</h2>
+      <div className="portfolio-container-other">
+        <div className="portfolio-contents-container">
+          <h2 className="portpolio-title ">ProYoga Buddy Website</h2>
           <div className="portfolio-detail-container items-end">
-            <span className="portpolio_tag w-[182px]">Website Design</span>
-            <span className="portpolio_tag w-[227px]">React Development</span>
-            <span className="portpolio_tag w-[109px]">Concept</span>
-            <StyledLink href="/" className="portpolio_link">
+            <span className="portpolio-tag w-[182px]">Website Design</span>
+            <span className="portpolio-tag w-[227px]">React Development</span>
+            <span className="portpolio-tag w-[109px]">Concept</span>
+            <StyledLink href="/" className="portpolio-link">
               see this project
             </StyledLink>
           </div>
         </div>
         <div className="mt-[10px]">
           <Image
-            className="portpolio_img"
+            className="portpolio-img"
             src={proyoga_portfolio}
             alt="proyoga_portfolio"
           />
@@ -103,38 +101,38 @@ export default function Portpolio() {
       <div className="portfolio-container">
         <div className="mt-[10px]">
           <Image
-            className="portpolio_img"
+            className="portpolio-img"
             src={cryptomoney_portfolio}
             alt="cryptomoney_portfolio"
           />
         </div>
         <div>
-          <h2 className="portpolio_title">CryptoMoney Website</h2>
+          <h2 className="portpolio-title">CryptoMoney Website</h2>
           <div className="portfolio-detail-container">
-            <span className="portpolio_tag w-[182px]">Website Design</span>
-            <span className="portpolio_tag w-[227px]">React Development</span>
-            <span className="portpolio_tag w-[109px]">Concept</span>
-            <StyledLink href="/" className="portpolio_link">
+            <span className="portpolio-tag w-[182px]">Website Design</span>
+            <span className="portpolio-tag w-[227px]">React Development</span>
+            <span className="portpolio-tag w-[109px]">Concept</span>
+            <StyledLink href="/" className="portpolio-link">
               see this project
             </StyledLink>
           </div>
         </div>
       </div>
-      <div className="portfolio-container justify-end my-[36px]">
-        <div>
-          <h2 className="portpolio_title">Portfolio Website</h2>
+      <div className="portfolio-container-other">
+        <div className="portfolio-contents-container">
+          <h2 className="portpolio-title">Portfolio Website</h2>
           <div className="portfolio-detail-container items-end">
-            <span className="portpolio_tag w-[182px]">Website Design</span>
-            <span className="portpolio_tag w-[227px]">React Development</span>
-            <span className="portpolio_tag w-[109px]">Concept</span>
-            <StyledLink href="/" className="portpolio_link">
+            <span className="portpolio-tag w-[182px]">Website Design</span>
+            <span className="portpolio-tag w-[227px]">React Development</span>
+            <span className="portpolio-tag w-[109px]">Concept</span>
+            <StyledLink href="/" className="portpolio-link">
               see this project
             </StyledLink>
           </div>
         </div>
         <div className="mt-[10px]">
           <Image
-            className="portpolio_img"
+            className="portpolio-img"
             src={portfolio_portfolio}
             alt="portfolio_portfolio"
           />
@@ -143,38 +141,38 @@ export default function Portpolio() {
       <div className="portfolio-container">
         <div className="mt-[10px]">
           <Image
-            className="portpolio_img"
+            className="portpolio-img"
             src={travel_portfolio}
             alt="travel_portfolio"
           />
         </div>
         <div>
-          <h2 className="portpolio_title">Travel Website</h2>
+          <h2 className="portpolio-title">Travel Website</h2>
           <div className="portfolio-detail-container">
-            <span className="portpolio_tag w-[182px]">Website Design</span>
-            <span className="portpolio_tag w-[227px]">React Development</span>
-            <span className="portpolio_tag w-[109px]">Concept</span>
-            <StyledLink href="/" className="portpolio_link">
+            <span className="portpolio-tag w-[182px]">Website Design</span>
+            <span className="portpolio-tag w-[227px]">React Development</span>
+            <span className="portpolio-tag w-[109px]">Concept</span>
+            <StyledLink href="/" className="portpolio-link">
               see this project
             </StyledLink>
           </div>
         </div>
       </div>
-      <div className="portfolio-container justify-end my-[36px]">
-        <div>
-          <h2 className="portpolio_title">Portfolio Website</h2>
+      <div className="portfolio-container-other portfolio-container-last">
+        <div className="portfolio-contents-container">
+          <h2 className="portpolio-title">Portfolio Website</h2>
           <div className="portfolio-detail-container items-end">
-            <span className="portpolio_tag w-[182px]">Website Design</span>
-            <span className="portpolio_tag w-[227px]">React Development</span>
-            <span className="portpolio_tag w-[109px]">Concept</span>
-            <StyledLink href="/" className="portpolio_link">
+            <span className="portpolio-tag w-[182px]">Website Design</span>
+            <span className="portpolio-tag w-[227px]">React Development</span>
+            <span className="portpolio-tag w-[109px]">Concept</span>
+            <StyledLink href="/" className="portpolio-link">
               see this project
             </StyledLink>
           </div>
         </div>
-        <div className="mt-[10px] mb-[104px]">
+        <div className="portpolio-last-image">
           <Image
-            className="portpolio_img"
+            className="portpolio-img"
             src={portfolio_portfolio1}
             alt="portfolio_portfolio1"
           />
